bee6db4cf4c908948a662bceafce6aabb4e2984c
Faizan Manzoor <faizan.manzoor@codebtech.com>
2025-04-17 02:48:55 +0500

    integar variables

---
+++ integar variable/main.dart
@@ -0,0 +1,20 @@
+main() {
+  //3.Create a integer variable num = 7, Add 8 to the number and then divide it by 3.
+
+  int num = 7;
+
+//addition of 8
+  num += 8;
+
+//division by 3
+  num ~/= 3;
+
+//modulus with 5
+  num %= 5;
+
+//multiply result by 5
+
+  int reuslt = num * 5;
+
+  print("The final result is: $reuslt");
+}
