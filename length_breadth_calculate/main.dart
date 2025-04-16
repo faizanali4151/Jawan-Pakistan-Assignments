cbde6f6f268611e7ed81759dee183f8233f1226d
Faizan Manzoor <faizan.manzoor@codebtech.com>
2025-04-17 02:42:59 +0500

    calculate length and breadth

---
+++ length_breadth_calculate/main.dart
@@ -0,0 +1,9 @@
+main() {
+// //2.Length and breadth of a rectangle are 5 and 7 respectively.
+
+  int length = 5;
+  int breadth = 7;
+  int areaOfRectangle = length * breadth;
+
+  print('The area of the Rectangle is $areaOfRectangle');
+}
